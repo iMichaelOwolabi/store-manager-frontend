f4f67f6a36c4535dc68f04b4bcb8fb599ce3bbbf
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-11-23 00:03:25 +0100

    add input validation file

---
+++ client/js/validations.js
@@ -0,0 +1,54 @@
+class Validations {
+  static validNumber(input) {
+    const validInput = /^[1-9]\d*$/;
+    const validate = validInput.test(input);
+    if (!validate) {
+      return false;
+    }
+    return true;
+  }
+
+  static validQuantity(input) {
+    if (typeof input !== 'number') {
+      return false;
+    }
+    const validInput = /^[1-9]\d*$/;
+    const validate = validInput.test(input);
+    if (!validate) {
+      return false;
+    }
+    return true;
+  }
+
+  static validPrice(input) {
+    if (typeof input !== 'number') {
+      return false;
+    }
+    const validInput = /^[1-9]\d*(\.\d*)?$/;
+    const validate = validInput.test(input);
+    if (!validate) {
+      return false;
+    }
+    return true;
+  }
+
+  static acceptableString(input) {
+    const validInput = /^[A-Za-z]\d*/;
+    const validate = validInput.test(input || '');
+    if (!validate) {
+      return false;
+    }
+    return true;
+  }
+
+  static validImageUrl(input) {
+    const validInput = /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/;
+    const validate = validInput.test(input);
+    if (!validate) {
+      return false;
+    }
+    return true;
+  }
+}
+
+export default Validations;
